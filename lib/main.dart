7e2b9ba172cd1acbf8b0f65d9e1e5cd8efc27d37
sarthak arya <sarthakarya1068a@gmail.com>
2025-03-28 22:12:28 +0530

    Fixed navigation bar and improved UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'screens/login_screen.dart';
 import 'screens/home_screen.dart';
 import 'screens/qr_generator_screen.dart';
 import 'screens/qr_scanner_screen.dart';
 import 'screens/attendance_records_screen.dart';
+import 'screens/profile_screen.dart'; // New Profile Screen
 
 void main() {
   runApp(AttendEaseApp());
@@ -15,14 +15,61 @@ class AttendEaseApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'AttendEase',
-      initialRoute: '/',
-      routes: {
-        '/': (context) => LoginScreen(),
-        '/home': (context) => HomeScreen(),
-        '/generate_qr': (context) => QRGeneratorScreen(),
-        '/scan_qr': (context) => QRScannerScreen(),
-        '/attendance_records': (context) => AttendanceRecordsScreen(),
-      },
+      home: MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _selectedIndex = 0;
+
+  // List of screens
+  final List<Widget> _screens = [
+    HomeScreen(),
+    QRGeneratorScreen(),
+    QRScannerScreen(),
+    AttendanceRecordsScreen(),
+    ProfileScreen(), // Added Profile Page
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.grey,
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.qr_code),
+            label: 'Generate QR',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.camera_alt),
+            label: 'Scan QR',
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.list), label: 'Records'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ), // New Profile Tab
+        ],
+      ),
     );
   }
 }
---
+++ lib/screens/profile_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Profile')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.person, size: 100, color: Colors.blue),
+            SizedBox(height: 20),
+            Text(
+              'Your Profile',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                // Add settings functionality here
+              },
+              child: Text('Settings'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
